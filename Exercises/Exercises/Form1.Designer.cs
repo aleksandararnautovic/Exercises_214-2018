5fb70fe0692bc863b184c93df9e922ce496a618b
PSEUDOLANGUGES\Pseudo_Languages <pseudolanguage@gmail.com>
2020-11-09 22:09:21 +0100

    Kreirane f-je za unos i dodavanje podataka u tabelu

--- Exercises/Exercises/Form1.Designer.cs
+++ Exercises/Exercises/Form1.Designer.cs
@@ -30,8 +30,12 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.Index = new System.Windows.Forms.Label();
+            this.Point = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtIndex = new System.Windows.Forms.TextBox();
+            this.txtPoint = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -47,37 +51,72 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Location = new System.Drawing.Point(23, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Name";
             // 
-            // label2
+            // Index
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 8);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 25);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.Index.AutoSize = true;
+            this.Index.Location = new System.Drawing.Point(12, 157);
+            this.Index.Name = "Index";
+            this.Index.Size = new System.Drawing.Size(55, 25);
+            this.Index.TabIndex = 2;
+            this.Index.Text = "Index";
             // 
-            // label3
+            // Point
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 16);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 25);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.Point.AutoSize = true;
+            this.Point.Location = new System.Drawing.Point(12, 292);
+            this.Point.Name = "Point";
+            this.Point.Size = new System.Drawing.Size(52, 25);
+            this.Point.TabIndex = 3;
+            this.Point.Text = "Point";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(23, 41);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(150, 31);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtIndex
+            // 
+            this.txtIndex.Location = new System.Drawing.Point(12, 185);
+            this.txtIndex.Name = "txtIndex";
+            this.txtIndex.Size = new System.Drawing.Size(150, 31);
+            this.txtIndex.TabIndex = 5;
+            // 
+            // txtPoint
+            // 
+            this.txtPoint.Location = new System.Drawing.Point(12, 320);
+            this.txtPoint.Name = "txtPoint";
+            this.txtPoint.Size = new System.Drawing.Size(150, 31);
+            this.txtPoint.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 631);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPoint);
+            this.Controls.Add(this.txtIndex);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.Point);
+            this.Controls.Add(this.Index);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -92,8 +131,12 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Index;
+        private System.Windows.Forms.Label Point;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtIndex;
+        private System.Windows.Forms.TextBox txtPoint;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Exercises/Exercises/Form1.cs
+++ Exercises/Exercises/Form1.cs
@@ -59,5 +59,21 @@ namespace Exercises
                     exerciseResult1.StudentIndex + " " + exerciseResult1.Point);
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection konkcija = new SqlConnection())
+            {
+                konkcija.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB214-2018;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                konkcija.Open();
+
+                string q = String.Format("INSERT into ExerciseResults values('{0}','{1}','{2}')",
+                txtName.Text, txtIndex.Text, Convert.ToInt32(txtPoint.Text));
+
+                SqlCommand komanda = new SqlCommand(q, konkcija);
+                komanda.ExecuteNonQuery();
+
+            }
+        }
     }
 }
